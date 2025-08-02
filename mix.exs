4709103de7af68fbe91098723ed65fa3eb6c2e21
Daniel Morandini <danielmorandini@me.com>
2025-08-02 22:33:45 +0200

    Prepare for publication

--- mix.exs
+++ mix.exs
@@ -6,14 +6,15 @@ defmodule MCP.MixProject do
       app: :mcp,
       version: "0.1.0",
       elixir: "~> 1.18",
-      start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/kim-company/mcp",
+      name: "MCP Server implementation",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
-
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -28,7 +29,26 @@ defmodule MCP.MixProject do
     [
       {:plug, "~> 1.15"},
       {:bandit, ">= 0.0.0", only: :test},
-      {:req, ">= 0.5.0", only: :test}
+      {:req, ">= 0.5.0", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  defp package do
+    [
+      maintainers: ["KIM Keep In Mind"],
+      files: ~w(lib mix.exs README.md LICENSE),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/kim-company/mcp"}
+    ]
+  end
+
+  defp description do
+    """
+    Elixir implementation of Model Context Protocol (MCP) server with Server-Sent Events transport
+    """
   end
 end
